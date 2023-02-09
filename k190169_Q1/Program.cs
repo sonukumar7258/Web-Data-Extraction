c785f4315d33f0329920df05df0fa6020d96de06
Sonu Kumar <sonukumarchawlaskc@gmail.com>
2023-02-10 04:07:50 +0500

    Initial Commit

---
+++ k190169_Q1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace k190169_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] months = {"Jan", "Feb", "Mar", "Apr", "May",
+                                "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"};
+            Console.WriteLine("Enter Url: ");
+            String url = Console.ReadLine();
+            Console.WriteLine("Enter Location of Specified Folder: ");
+            String finalFolder = Console.ReadLine();
+
+            // Create a new WebClient instance.
+            WebClient myWebClient = new WebClient();
+
+            Console.WriteLine("Downloading Html Data----");
+            // DownloadHtml Format of that webpage and treated as String and Store into the data
+            String data = myWebClient.DownloadString(url);
+
+            DateTime now = DateTime.Now;
+            // get the todays date 
+            // String todaysDate = now.Day.ToString() + months[now.Month - 1] + now.Year.ToString(); // by this we can generate todaydate named file
+            String todaysDate = "16Oct22";
+            // Set the name of folder of todays date
+            finalFolder += "\\" + "Summary" + todaysDate + ".html";
+
+            // Store String into HTML File
+            File.WriteAllText(finalFolder, data);
+
+            Console.WriteLine("File Downloaded Successfully at Location : {0} from {1} ", finalFolder, url);
+
+        }
+    }
+}
